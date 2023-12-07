ee6950985a1eee7ce3cbbd8b8ccd547490cba62d
Harshit SIngh <hs20242477@gmail.com>
2023-12-07 11:27:35 +0530

    changes according to #1

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
  
  //  My first solidity code which I wrote at a workshop conducted by BlocSoc IITR.
 
-//just learning stuff
+ //just learning stuff
+ //bruhhh
+ //sed
+ //work pls
--- contracts...1_Storage.sol
+++ contracts...1_Storage.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.7;
 
 contract Storage {
     uint256 public myNum;
+    uint256 public noNum;
 
     function store(uint256 newNum) public {
         myNum=newNum;
@@ -14,5 +15,7 @@ contract Storage {
     }
 
 //yes
+
+//this shit wack
 
 }
